d83ae8b2e534f74639fbb524c4b09eca03f05f80
Sharayah Hefner <rocknrayah@gmail.com>
2022-07-15 14:06:46 -0600

    player movement

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ class Player {
   }
   update() {
     this.draw()
+    this.position.x += this.velocity.x
     this.position.y += this.velocity.y
     
     if (this.position.y + this.height + this.velocity.y <= canvas.height)
@@ -33,11 +34,69 @@ class Player {
   }
 }
 const player = new Player()
+const keys = {
+  right: {
+  pressed: false
+  },
+  left: {
+  pressed: false
+  },
+}
 
 
 function animate() {
   requestAnimationFrame(animate)
   c.clearRect(0, 0, canvas.width, canvas.height)
   player.update()
+  if (keys.right.pressed) {
+  player.velocity.x = 5
+  } else if(keys.left.pressed) {
+  player.velocity.x = -5
+  } else player.velocity.x = 0
 }
 animate()
+
+addEventListener('keydown',({keyCode}) => {
+  // console.log(keyCode)
+  switch(keyCode) {
+    case 37:
+      console.log('left')
+      keys.left.pressed = true
+      
+      break
+    case 40:
+      console.log('down')
+      break
+      case 39:
+      console.log('right')
+      keys.right.pressed = true
+      break
+      case 38:
+      console.log('up')
+      player.velocity.y -= 20
+      break
+  }
+  console.log(keys.right.pressed)
+})
+addEventListener('keyup',({keyCode}) => {
+  // console.log(keyCode)
+  switch(keyCode) {
+    case 37:
+      console.log('left')
+      keys.left.pressed = false
+      break
+    case 40:
+      console.log('down')
+      break
+      case 39:
+      console.log('right')
+      keys.right.pressed = false
+      break
+      case 38:
+      console.log('up')
+      player.velocity.y -= 20
+      break
+  }
+  console.log(keys.right.pressed)
+  
+})
